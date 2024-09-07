c1f99e5f1339f7aa3c4a3d23af8da628d3efce33
Ricy <ricyzhang951@gmail.com>
2024-09-07 11:59:53 -0700

    fix: promise.js

--- JS/functions/promise.js
+++ JS/functions/promise.js
@@ -4,12 +4,13 @@ const STATUS = {
     REJECTED: "rejected",
 };
 
-class CustomPromise {
+class MyPromise {
     constructor(executor) {
         // Initial state
         this.state = STATUS.PENDING; // Can be 'pending', 'fulfilled', or 'rejected'
-        this.value = undefined;
-        this.callbacks = []; // To handle then/catch chaining
+        this.value = null;
+        this.onFulfilledCallbacks = []; // To handle then chaining
+        this.onRejectedCallbacks = [];
 
         // Resolve function
         const resolve = (value) => {
@@ -18,17 +19,21 @@ class CustomPromise {
             this.value = value;
 
             // Call all stored success handlers
-            this.callbacks.forEach((callback) => callback.onFulfilled(value));
+            this.onFulfilledCallbacks.forEach((callback) =>
+                callback.onFulfilled(value)
+            );
         };
 
         // Reject function
         const reject = (reason) => {
             if (this.state !== STATUS.PENDING) return;
-            this.state = status.rejected;
+            this.state = STATUS.REJECTED;
             this.value = reason;
 
             // Call all stored failure handlers
-            this.callbacks.forEach((callback) => callback.onRejected(reason));
+            this.onRejectedCallbacks.forEach((callback) =>
+                callback.onRejected(reason)
+            );
         };
 
         // Execute the executor function with resolve and reject
@@ -41,75 +46,61 @@ class CustomPromise {
 
     // then() function
     then(onFulfilled, onRejected) {
-        return new CustomPromise((resolve, reject) => {
-            // Success handler
-            const handleFulfilled = (value) => {
-                try {
-                    const result = onFulfilled ? onFulfilled(value) : value;
-                    resolve(result);
-                } catch (error) {
-                    reject(error);
-                }
-            };
-
-            // Failure handler
-            const handleRejected = (reason) => {
-                try {
-                    const result = onRejected ? onRejected(reason) : reason;
-                    reject(result);
-                } catch (error) {
-                    reject(error);
-                }
-            };
-
-            // If the promise is already settled, handle it immediately
-            if (this.state === status.fulfilled) {
-                handleFulfilled(this.value);
-            } else if (this.state === status.rejected) {
-                handleRejected(this.value);
-            } else {
-                // If the promise is still pending, store the handlers
-                this.callbacks.push({
-                    onFulfilled: handleFulfilled,
-                    onRejected: handleRejected,
-                });
-            }
-        });
+        onFulfilled =
+            typeof onFulfilled === "function" ? onFulfilled : (value) => value;
+        onRejected =
+            typeof onRejected === "function"
+                ? onRejected
+                : (reason) => {
+                      throw reason;
+                  };
+        if (this.state === STATUS.FULFILLED) {
+            onFulfilled(this.value);
+        } else if (this.state === STATUS.REJECTED) {
+            onRejected(this.reason);
+        } else {
+            this.onFulfilledCallbacks.push(onFulfilled);
+            this.onRejectedCallbacks.push(onRejected);
+        }
+        return this;
     }
 
     // catch() function
     catch(onRejected) {
         return this.then(null, onRejected);
     }
+
+    static all(promises) {
+        return new MyPromise((resolve, reject) => {
+            let results = [];
+            let completed = 0;
+            promises.forEach((promise, index) => {
+                MyPromise.resolve(promise)
+                    .then((value) => {
+                        results[index] = value;
+                        completed++;
+                        if (completed === promises.length) {
+                            resolve(results);
+                        }
+                    })
+                    .catch(reject);
+            });
+        });
+    }
+
+    static race(promises) {
+        return new MyPromise((resolve, reject) => {
+            promises.forEach((promise) => {
+                MyPromise.resolve(promise).then(resolve).catch(reject);
+            });
+        });
+    }
+
+    static resolve(value) {
+        return new MyPromise((resolve) => resolve(value));
+    }
+
+    static reject(reason) {
+        return new MyPromise((_, reject) => reject(reason));
+    }
 }
-
-// Promise.all(promiseArr)
-CustomPromise.all = function (promises) {
-    return new CustomPromise((resolve, reject) => {
-        let resolvedValues = [];
-        let remaining = promises.length;
-
-        promises.forEach((promise, index) => {
-            CustomPromise.resolve(promise)
-                .then((value) => {
-                    resolvedValues[index] = value;
-                    remaining--;
-
-                    // If all promises are resolved, resolve the returned promise
-                    if (remaining === 0) {
-                        resolve(resolvedValues);
-                    }
-                })
-                .catch(reject); // If any promise fails, reject immediately
-        });
-    });
-};
-
-// Promise.race(promiseArr)
-CustomPromise.race = function (promises) {
-    return new CustomPromise((resolve, reject) => {
-        promises.forEach((promise) => {
-            CustomPromise.resolve(promise).then(resolve).catch(reject);
-        });
-    });
-};
