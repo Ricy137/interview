749a3ee7ef317b3dd3991668e9627364ee89ecf1
Ricy <ricyzhang951@gmail.com>
2024-09-21 11:07:41 -0700

    fix: reduce

--- JS/functions/reduce.js
+++ JS/functions/reduce.js
@@ -1,13 +1,4 @@
 // [reduce](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce)
-Array.prototype.myMapWReduce = function (fn, thisValue) {
-    let res = [];
-    thisValue = thisValue ?? [];
-    this.reduce(function (pre, cur, index, arr) {
-        return res.push(fn.call(thisValue, cur, index, arr));
-    }, []);
-    return res;
-};
-
 const reduce = (arr, cb, initialValue) => {
     let num = initialValue == undefined ? arr[0] : initialValue;
     let i = initialValue == undefined ? 1 : 0;
@@ -24,5 +15,3 @@ function sum(pre, cur, index) {
 let arr = [1, 2, 3, 4];
 let b = reduce(arr, sum);
 console.log("b: ", b);
-const c = a.myMapWReduce((a) => a * 10);
-console.log("c", c);
