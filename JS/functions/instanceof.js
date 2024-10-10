7ade2f8b9a2448782254a6090030329cbcf7f9b3
Ricy <ricyzhang951@gmail.com>
2024-10-10 11:57:19 -0400

    refactor: instanceof

--- JS/functions/instanceof.js
+++ JS/functions/instanceof.js
@@ -1,9 +1,11 @@
 // The `instanceof` operator allows to check whether an object belongs to a certain class. Inheritence is considered
 // checking the prototype of the constructor exists in the object's prototype chain
 var myInstanceof = function (target, origin) {
-    if ((typeof target !== "object" && typeof target !== "function") ||
-        target == null)
-        return false;
+    // if (
+    //     (typeof target !== "object" && typeof target !== "function") ||
+    //     target == null
+    // )
+    //     return false;
     var proto = Object.getPrototypeOf(target);
     while (proto) {
         if (proto === origin.prototype) {
@@ -17,4 +19,6 @@ var a = [1, 2, 3];
 console.log(myInstanceof(a, Array)); //true
 console.log(myInstanceof(a, Object)); //true
 console.log(myInstanceof(a, String)); //false
-console.log(myInstanceof(function () { }, Function)); // true// true
+console.log(myInstanceof(function () { }, Function)); // true
+console.log(myInstanceof(function () { }, 1)); // false
+console.log(myInstanceof(1, 1)); // false
--- JS/functions/instanceof.ts
+++ JS/functions/instanceof.ts
@@ -1,11 +1,11 @@
 // The `instanceof` operator allows to check whether an object belongs to a certain class. Inheritence is considered
 // checking the prototype of the constructor exists in the object's prototype chain
 const myInstanceof = (target: any, origin: any) => {
-    if (
-        (typeof target !== "object" && typeof target !== "function") ||
-        target == null
-    )
-        return false;
+    // if (
+    //     (typeof target !== "object" && typeof target !== "function") ||
+    //     target == null
+    // )
+    //     return false;
     let proto = Object.getPrototypeOf(target);
     while (proto) {
         if (proto === origin.prototype) {
@@ -21,3 +21,5 @@ console.log(myInstanceof(a, Array)); //true
 console.log(myInstanceof(a, Object)); //true
 console.log(myInstanceof(a, String)); //false
 console.log(myInstanceof(() => {}, Function)); // true
+console.log(myInstanceof(() => {}, 1)); // false
+console.log(myInstanceof(1, 1)); // false
