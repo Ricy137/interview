a4feba150433486e1007e0a4148d2471224b668b
Ricy <ricyzhang951@gmail.com>
2024-10-10 11:55:09 -0400

    fix: instanceof for functions

--- JS/functions/instanceof.js
+++ JS/functions/instanceof.js
@@ -1,12 +1,10 @@
 // The `instanceof` operator allows to check whether an object belongs to a certain class. Inheritence is considered
 // checking the prototype of the constructor exists in the object's prototype chain
-const myInstanceof = (target: any, origin: any) => {
-    if (
-        (typeof target !== "object" && typeof target !== "function") ||
-        target == null
-    )
+var myInstanceof = function (target, origin) {
+    if ((typeof target !== "object" && typeof target !== "function") ||
+        target == null)
         return false;
-    let proto = Object.getPrototypeOf(target);
+    var proto = Object.getPrototypeOf(target);
     while (proto) {
         if (proto === origin.prototype) {
             return true;
@@ -15,9 +13,8 @@ const myInstanceof = (target: any, origin: any) => {
     }
     return false;
 };
-
-let a = [1, 2, 3];
+var a = [1, 2, 3];
 console.log(myInstanceof(a, Array)); //true
 console.log(myInstanceof(a, Object)); //true
 console.log(myInstanceof(a, String)); //false
-console.log(myInstanceof(() => {}, Function)); // true
+console.log(myInstanceof(function () { }, Function)); // true// true
