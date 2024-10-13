41e957a7622078d4d4deb1d58da9eb50ab45c76e
Ricy <ricyzhang951@gmail.com>
2024-10-13 14:04:44 -0700

    feat: add click shift down

--- vanilla/sudoku/src/index.js
+++ vanilla/sudoku/src/index.js
@@ -1,60 +1,55 @@
 class Finder {
     constructor() {}
 
-    // Find the specific element with given color
-    find(color) {
-        // query all elements with class .square
-        const squares = document.querySelectorAll(".square");
-        //filtered out the one with specific bg color
-        let targets = [];
-        for (let square of squares) {
-            if (window.getComputedStyle(square).backgroundColor === color) {
-                targets.push(square);
-            }
-        }
-        // return the filtered result
-        return targets;
-    }
+    find = (color) => {
+        return Array.from(document.querySelectorAll(".square")).find(
+            (cell) => window.getComputedStyle(cell).backgroundColor === color
+        );
+    };
 
-    // Change the background color of the given element
-    setColor(ele, newColor) {
+    setColor = (ele, newColor) => {
+        if (!ele || !newColor) return;
         ele.style.backgroundColor = newColor;
-        //debugger;
-    }
+    };
 
-    // shiftdown
-    shiftDown(ele) {
-        const parent = ele.parentNode;
-        const nextSilibing = ele.nextElementSibling;
-        //ele.remove();
-        //debugger;
-        if (!nextSilibing) {
-            parent.insertBefore(ele, parent.firstChild);
-        } else if (!nextSilibing.nextSilibing) {
-            parent.appendChild(ele);
+    shiftDown = (ele) => {
+        if (!ele) return;
+        const parentNode = ele.parentNode;
+        const sibling = ele.nextElementSibling;
+        const nextSibling = ele.nextElementSibling?.nextElementSibling;
+        if (!sibling) {
+            //debugger;
+            parentNode.insertBefore(ele, parentNode.firstChild);
+        } else if (!nextSibling) {
+            parentNode.append(ele);
         } else {
-            parent.insertBefore(ele, nextSilibing.nextSilibing);
+            parentNode.insertBefore(ele, nextSibling);
         }
-    }
+    };
+
+    printout = () => {
+        Array.from(document.querySelectorAll(".square")).forEach((square) => {
+            square.addEventListener("click", () => {
+                console.log(window.getComputedStyle(square).backgroundColor);
+            });
+        });
+    };
+
+    shiftDownClicking = () => {
+        Array.from(document.querySelectorAll(".square")).forEach((square) => {
+            square.addEventListener("click", () => {
+                const clickedColor =
+                    window.getComputedStyle(square).backgroundColor;
+                const clickedEle = this.find(clickedColor);
+                this.shiftDown(clickedEle);
+            });
+        });
+    };
 }
 
-// printout the cell clicksed
-const printoutClickCell = () => {
-    const cells = document.querySelectorAll(".square");
-    cells.forEach((cell) =>
-        cell.addEventListener("click", () => {
-            console.log(
-                "clicked: ",
-                window.getComputedStyle(cell).backgroundColor
-            );
-        })
-    );
-};
-
-printoutClickCell();
-
-// test: turn the red color to black and then shiftdown
 const finder = new Finder();
-const redSqure = finder.find("rgb(255, 80, 0)")[0];
-finder.setColor(redSqure, "#000000");
-finder.shiftDown(redSqure);
+finder.printout();
+const target = finder.find("rgb(179, 145, 11)");
+finder.setColor(target, "#000000");
+finder.shiftDown(target);
+finder.shiftDownClicking();
